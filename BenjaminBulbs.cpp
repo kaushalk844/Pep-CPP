a46ac2e54b38c5aa4cf209bdd1eab704de994d35
Kaushal Rajput <kaushalk844@gmail.com>
2021-02-28 22:54:57 +0530

    Add files via upload

---
+++ BenjaminBulbs.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    //write code here
+    for (int i = 1; i * i <= n; i++)
+    {
+        cout<<i*i<<endl;
+    }
+    
+}
